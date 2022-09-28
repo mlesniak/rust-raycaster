0fe5a5a81426e955547673623de5c355184efecc
Michael Lesniak <mail@mlesniak.com>
2022-09-28 21:17:31 +0200

    Add mouse react playground

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,9 @@ fn main() -> Result<(), String> {
     canvas.clear();
     canvas.present();
 
+    // TODO(mlesniak) Have a map with flags?
+    let mut change_color = false;
+    let mut time_passed = 0;
     'main_loop: loop {
         for event in event_pump.poll_iter() {
             match event {
@@ -30,11 +33,24 @@ fn main() -> Result<(), String> {
                     keycode: Some(Keycode::Escape),
                     ..
                 } => break 'main_loop,
+                Event::MouseButtonDown { .. } => change_color = true,
+                Event::MouseButtonUp { .. } => change_color = false,
                 _ => {}
             }
         }
 
         canvas.clear();
+
+        if change_color && time_passed > 10 {
+            time_passed = 0;
+            canvas.set_draw_color(Color::RGB(
+                rand::random::<u8>(),
+                rand::random::<u8>(),
+                rand::random::<u8>(),
+            ));
+        }
+        time_passed = time_passed + 1;
+
         canvas.present();
 
         // Non-adaptive FPS duration.
