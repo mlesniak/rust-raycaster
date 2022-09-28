8dd2b8d7c55b5a7db22fc50c4e742cd8ad9ddb73
Michael Lesniak <mail@mlesniak.com>
2022-09-28 21:31:19 +0200

    Draw pixels of specific color

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
+use sdl2::rect::Point;
+use sdl2::*;
 use std::time::Duration;
 
 fn main() -> Result<(), String> {
-    let sdl_context = sdl2::init()?;
+    let sdl_context = init()?;
     let video_subsystem = sdl_context.video()?;
 
     let window = video_subsystem
@@ -23,6 +25,7 @@ fn main() -> Result<(), String> {
     canvas.present();
 
     // TODO(mlesniak) Have a map with flags?
+    // TODO(mlesniak) Add very simple trait with basic operations
     let mut change_color = false;
     let mut time_passed = 0;
     'main_loop: loop {
@@ -39,8 +42,6 @@ fn main() -> Result<(), String> {
             }
         }
 
-        canvas.clear();
-
         if change_color && time_passed > 10 {
             time_passed = 0;
             canvas.set_draw_color(Color::RGB(
@@ -50,6 +51,14 @@ fn main() -> Result<(), String> {
             ));
         }
         time_passed += 1;
+        canvas.clear();
+
+        let c = canvas.draw_color();
+        canvas.set_draw_color(Color::RED);
+        for x in 300..320 {
+            canvas.draw_point(Point::new(x, 300))?;
+        }
+        canvas.set_draw_color(c);
 
         canvas.present();
 
