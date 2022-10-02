67eee8a75701eb5a9bd316dd390c92fecb1382e7
Michael Lesniak <mail@mlesniak.com>
2022-10-02 15:06:13 +0200

    Use faster random number generator

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use rand::random;
 use std::collections::HashMap;
-use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use std::time::{Duration, Instant, SystemTime, UNIX_EPOCH};
 
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
@@ -9,10 +8,12 @@ use sdl2::rect::{Point, Rect};
 use sdl2::render::WindowCanvas;
 use sdl2::*;
 
-// const WIDTH: u32 = 2560;
-// const HEIGHT: u31 = 1440;
-const WIDTH: u32 = 640;
-const HEIGHT: u32 = 360;
+use tinyrand::{Rand, StdRand};
+
+const WIDTH: u32 = 2560;
+const HEIGHT: u32 = 1440;
+// const WIDTH: u32 = 640;
+// const HEIGHT: u32 = 360;
 const LENGTH: usize = (WIDTH * HEIGHT) as usize * 4;
 const FPS: u128 = 60;
 
@@ -40,19 +41,21 @@ fn main() -> Result<(), String> {
         pixels.push(0);
     }
 
+    let mut rng = StdRand::default();
+
     let mut c: u8 = 0;
     let mut dir: i32 = 1;
     loop {
-        let d1 = SystemTime::now();
+        let timer = Instant::now();
         if event_handling(&mut event_pump) {
             break;
         }
         background(canvas);
 
         for i in (0..(WIDTH * HEIGHT * 4) as usize).step_by(4) {
-            pixels[i] = c;
-            pixels[i + 1] = c / 2;
-            pixels[i + 2] = c / 3;
+            pixels[i] = (rng.next_u64() % 256) as u8;
+            pixels[i + 1] = (rng.next_u64() % 256) as u8;
+            pixels[i + 2] = (rng.next_u64() % 256) as u8;
         }
         c = (c as i32 + dir) as u8;
         if c == 255 || c == 0 {
@@ -64,16 +67,8 @@ fn main() -> Result<(), String> {
         canvas.copy(&tx, None, None)?;
         canvas.present();
 
-        // let d2 = SystemTime::now().duration_since(d1).unwrap().as_millis();
-        // if d2 < FPS {
-        //     std::thread::sleep(Duration::new(0, (FPS - d2) as u32 * 1_000));
-        // }
-        let d3 = SystemTime::now().duration_since(d1).unwrap().as_millis();
-        // if d3 > 20 {
-        // if c % 10 == 0 {
-        println!("{}", d3);
-        // }
-        // }
+        let diff = Instant::now().duration_since(timer);
+        println!("{}", diff.as_millis());
     }
 
     Ok(())
@@ -84,6 +79,7 @@ fn background(canvas: &mut WindowCanvas) {
     canvas.clear();
 }
 
+/// Return true if we shall quit.
 fn event_handling(event_pump: &mut EventPump) -> bool {
     for event in event_pump.poll_iter() {
         match event {
@@ -97,48 +93,4 @@ fn event_handling(event_pump: &mut EventPump) -> bool {
     }
 
     return false;
-}
-
-fn render_random_points(canvas: &mut WindowCanvas) -> Result<(), String> {
-    let mut map: HashMap<Color, Vec<Point>> = HashMap::new();
-
-    for x in 0..100 {
-        for y in 0..100 {
-            let color = Color::RGB(
-                rand::random::<u8>() % 10 + 150,
-                rand::random::<u8>() % 10 + 50,
-                rand::random::<u8>() % 10 + 150,
-            );
-            if !map.contains_key(&color) {
-                map.insert(color, Vec::new());
-            }
-
-            let mut v = map.get_mut(&color).unwrap();
-            v.push(Point::new(x, y));
-        }
-    }
-
-    println!("{}", map.len());
-
-    for (c, ps) in map.iter() {
-        canvas.set_draw_color(*c);
-        canvas.draw_points(ps.as_slice())?;
-    }
-
-    Ok(())
-}
-
-fn _render_sinus(canvas: &mut WindowCanvas) -> Result<(), String> {
-    let scale = 50.0;
-    let stretch = 40.0;
-
-    canvas.set_draw_color(Color::RED);
-    for x in 0..WIDTH {
-        let xf = x as f64 / stretch;
-        let xi = x as i32;
-        let yi: i32 = (HEIGHT / 2) as i32 + (xf.sin() * scale) as i32;
-        canvas.draw_point(Point::new(xi, yi))?;
-    }
-
-    Ok(())
 }
