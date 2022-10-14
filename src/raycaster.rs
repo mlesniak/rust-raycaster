e4735f5ccca50c1cc3eea8ec038ea82889fce010
Michael Lesniak <mail@mlesniak.com>
2022-10-14 06:39:57 +0200

    Add basic data structures

--- src/raycaster.rs
+++ src/raycaster.rs
@@ -5,11 +5,38 @@ use sdl2::render::WindowCanvas;
 
 pub struct Raycaster {
     pub color: Color,
+    map: Vec<Vec<i32>>,
+    player: Player,
+}
+
+struct Player {
+    x: f32,
+    y: f32,
+    angle: f32,
 }
 
 impl Raycaster {
     pub fn new(color: Color) -> Raycaster {
-        Raycaster { color }
+        Raycaster {
+            color,
+            map: vec![
+                vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                vec![1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                vec![1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                vec![1, 0, 0, 1, 1, 0, 1, 0, 0, 1],
+                vec![1, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+                vec![1, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+                vec![1, 0, 0, 1, 0, 1, 1, 0, 0, 1],
+                vec![1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                vec![1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            ],
+            player: Player {
+                x: 2.0,
+                y: 2.0,
+                angle: 90.0,
+            },
+        }
     }
 
     pub fn draw(&mut self, canvas: &mut WindowCanvas) -> Result<(), String> {
